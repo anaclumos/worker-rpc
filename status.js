da81e3d6a9751f8aa95006313c2af27aebbed25a
Sunghyun Cho <mail@chosunghyun.com>
2022-07-29 17:26:26 +0900

    prompt layer for worker

---
+++ status.js
@@ -0,0 +1,6 @@
+const Status = {
+  UNINITIALISED: 0,
+  READY: 1,
+  COMPLETED: 2,
+  ERROR: 3,
+}
--- worker.js
+++ worker.js
@@ -1,23 +1,40 @@
-postMessage('Worker is ready!')
-
-let sab = undefined
-let int32 = undefined
-
-onmessage = function (e) {
-  console.log('WORKER.onMessage:', e.data)
-  if (e.data?.type === 'init') {
-    sab = e.data.sab
-    int32 = new Int32Array(sab)
-    console.log('Received SAB')
-    heartbeat()
-  }
-  postMessage(`I am a worker. Hello, ${JSON.stringify(e.data)}.`)
+const Status = {
+  UNINITIALISED: 0,
+  READY: 1,
+  COMPLETED: 2,
+  ERROR: 3,
 }
 
-function heartbeat() {
-  setInterval(() => {
-    Atomics.wait(int32, 0, 0) // doesn't beat if [0] is 0
-    console.log('WORKER.heartbeat:', Atomics.load(int32, 0))
-    Atomics.add(int32, 0, 1)
-  }, 1000)
+function sleep(ms) {
+  const end = Date.now() + ms
+  while (Date.now() < end) {}
+  return Date.now()
 }
+
+// polyfill layer for Worker.Prompt
+function prompt(msg) {
+  const sab = new SharedArrayBuffer(1024)
+  const int32 = new Int32Array(sab)
+  Atomics.store(int32, 0, Status.READY)
+  postMessage({
+    sharedArrayBuffer: sab,
+    func: 'prompt',
+    msg: msg,
+  })
+
+  // Waiting
+  Atomics.wait(int32, 0, Status.READY)
+
+  // Waiting complete, get the result
+  const ab = new ArrayBuffer(sab.byteLength)
+  const view = new Uint8Array(ab)
+  view.set(new Uint8Array(sab))
+  const decoder = new TextDecoder()
+  const string = decoder.decode(view)
+  console.log('WORKER recieved result:', string)
+}
+
+const timeToWait = Math.floor(Math.random() * 10000)
+console.log('WORKER.sleep', timeToWait)
+sleep(timeToWait)
+prompt('hello!')
